c2110589e840b4eb93a17855b1f04ba5bf35d930
panjuanjuan <skyannesky@163.com>
2017-10-14 21:09:52 +0800

    添加html模版文件

--- login/views.py
+++ login/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+# 登录功能
+def login(request):
+    return render(request, 'login.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
