05994b80ccfd3f68a5f681605260bcd600643f36
anne_dev <anne_dev@163.com>
2017-10-16 15:20:44 +0800

    添加模版文件

--- login/urls.py
+++ login/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index),
+    url(r'^index/$', views.index, name='index'),
     url(r'^list/$', views.list),
     url(r'^detail/$', views.detail),
     url(r'^cart/$', views.cart),
